327fc1cec3b49af34451fbd11d796969f2e62dfa
AkbarKarshiev <50545784+AkbarKarshiev@users.noreply.github.com>
2022-05-06 17:06:18 +0500

    9. Angular view Layer Patterns

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {Course, sortCoursesBySeqNo} from '../model/course';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
-import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
 
-import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
+import {Course, sortCoursesBySeqNo} from '../model/course';
 import { CoursesService } from "../services/courses.service";
 
 @Component({
@@ -17,7 +15,7 @@ export class HomeComponent implements OnInit {
   beginnerCourses$: Observable<Course[]>;
   advancedCourses$: Observable<Course[]>;
 
-  constructor(private coursesService: CoursesService, private dialog: MatDialog) {
+  constructor(private coursesService: CoursesService) {
 
   }
 
@@ -36,20 +34,6 @@ export class HomeComponent implements OnInit {
       .pipe(
         map(courses => courses.filter(course => course.category === "ADVANCED"))
       );
-  }
-
-  editCourse(course: Course) {
-
-    const dialogConfig = new MatDialogConfig();
-
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-    dialogConfig.width = "400px";
-
-    dialogConfig.data = course;
-
-    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
-
   }
 
 }
