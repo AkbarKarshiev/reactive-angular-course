e8b74bc48162d09ab8b75f02b182a451e2f876d8
AkbarKarshiev <50545784+AkbarKarshiev@users.noreply.github.com>
2022-05-06 17:34:55 +0500

    10. Data Modification Example

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,6 +20,10 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.reloadCourses()
+  }
+
+  reloadCourses(): void {
     const courses$ = this.coursesService.loadAllCourses()
       .pipe(
         map(courses => courses.sort(sortCoursesBySeqNo))
@@ -35,7 +39,6 @@ export class HomeComponent implements OnInit {
         map(courses => courses.filter(course => course.category === "ADVANCED"))
       );
   }
-
 }
 
 
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -13,9 +13,17 @@ export class CoursesService {
   constructor(private http: HttpClient) { }
 
   loadAllCourses(): Observable<Course[]> {
-    return this.http.get<Course[]>('/api/courses').pipe(
-      map(res => res['payload']),
-      shareReplay()
-    );
+    return this.http.get<Course[]>('/api/courses')
+      .pipe(
+        map(res => res['payload']),
+        shareReplay()
+      );
+  }
+
+  saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
+    return this.http.put(`/api/courses/${courseId}`, changes)
+      .pipe(
+        shareReplay()
+      );
   }
 }
